a861b12a69f08616f8ff0cd5127142c98ddc9544
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2013-10-10 09:58:47 +0200

    Update make.cmd

--- make.cmd
+++ make.cmd
@@ -1,2 +1,3 @@
 @"%~dp0tools\NuGet.exe" install IronPython.Interpreter -Version 2.7.4 -OutputDirectory "%~dp0tools" -NonInteractive -Verbosity quiet -ExcludeVersion
-@"%~dp0tools\IronPython.Interpreter\tools\ipy.exe" -tt "%~dp0tools\build.py" %*
+@"%~dp0tools\IronPython.Interpreter\tools\ipy.exe" -tt "%~dp0tools\make.py" "%~dp0makefile.py" %*
+
