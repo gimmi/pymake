8ea0f6009f96beb5ac9cd17d46377320bd97d7f6
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2013-10-24 18:27:55 +0200

    IronPython interpreted picked based on processor architecture

--- make.cmd
+++ make.cmd
@@ -1,3 +1,10 @@
-@"%~dp0tools\NuGet.exe" install IronPython.Interpreter -Version 2.7.4 -OutputDirectory "%~dp0tools" -NonInteractive -Verbosity quiet -ExcludeVersion
-@"%~dp0tools\IronPython.Interpreter\tools\ipy.exe" -tt "%~dp0tools\make.py" "%~dp0makefile.py" %*
-
+@ECHO OFF
+SETLOCAL
+"%~dp0tools\NuGet.exe" install IronPython.Interpreter -Version 2.7.4 -OutputDirectory "%~dp0tools" -NonInteractive -Verbosity quiet -ExcludeVersion
+if "%PROCESSOR_ARCHITECTURE%"=="x86" GOTO 32BIT
+	SET IRONPYTHON_EXE=ipy64.exe
+GOTO END
+	SET IRONPYTHON_EXE=ipy.exe
+:32BIT
+:END
+"%~dp0tools\IronPython.Interpreter\tools\%IRONPYTHON_EXE%" -tt "%~dp0tools\make.py" "%~dp0makefile.py" %*
