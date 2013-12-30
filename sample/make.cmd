6856882408f363333e631783c099d731d2aab947
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2013-12-30 09:16:50 +0100

    Delete make.cmd

--- sample/make.cmd
+++
@@ -1,2 +0,0 @@
-@NuGet.exe install IronPython.Interpreter -Version 2.7.3 -OutputDirectory "%~dp0..\tools" -NonInteractive -Verbosity quiet -ExcludeVersion
-@"%~dp0..\tools\IronPython.Interpreter\tools\ipy.exe" -tt "%~dp0..\make.py" "%~dp0makefile.py" %*
