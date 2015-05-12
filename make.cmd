a7cb5703716b621412b0745924d5918d6342104b
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2015-05-12 11:06:56 +0200

    Bootstrap script not consider WOW64
    see http://blogs.msdn.com/b/david.wang/archive/2006/03/26/howto-detect-process-bitness.aspx

--- make.cmd
+++ make.cmd
@@ -1,10 +1,11 @@
 @ECHO OFF
 SETLOCAL
 "%~dp0tools\NuGet.exe" install IronPython.Interpreter -Version 2.7.4 -OutputDirectory "%~dp0tools" -NonInteractive -Verbosity quiet -ExcludeVersion
-if "%PROCESSOR_ARCHITECTURE%"=="x86" GOTO 32BIT
-	SET IRONPYTHON_EXE=ipy64.exe
-	GOTO END
-:32BIT
-	SET IRONPYTHON_EXE=ipy.exe
-:END
+SET IRONPYTHON_EXE=ipy64.exe
+REM http://blogs.msdn.com/b/david.wang/archive/2006/03/26/howto-detect-process-bitness.aspx
+IF "%PROCESSOR_ARCHITECTURE%" == "x86" (
+    IF NOT DEFINED PROCESSOR_ARCHITEW6432 (
+        SET IRONPYTHON_EXE=ipy.exe
+    )
+)
 "%~dp0tools\IronPython.Interpreter\tools\%IRONPYTHON_EXE%" -tt "%~dp0tools\make.py" "%~dp0makefile.py" %*
